471bbbd263054c7ad5b76608ed8494bf7ac00c73
LiveIsBigStange <98121339+LiveIsBigStange@users.noreply.github.com>
2023-09-30 15:31:07 +0400

    first commit

---
+++ JavaMy/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 7;
+
+        x = 6;
+        System.out.println(1/0);
+
+        System.out.println(x);
+        System.out.println(y);
+//        System.out.println(x + y);
+    }
+}
