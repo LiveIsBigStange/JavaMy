827f5f6ba5efe96c60d79e8bfa7f5f3f068996ee
LiveIsBigStange <98121339+LiveIsBigStange@users.noreply.github.com>
2023-09-30 15:37:20 +0400

    Changes
    For novigating through the projects

--- First week/src/Main.java
+++ First week/src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 5;
-        int y = 7;
-
-        x = 6;
-        System.out.println(1/0);
+        int x;
+        x = 5;
+        int y = x + 1;
 
         System.out.println(x);
         System.out.println(y);
