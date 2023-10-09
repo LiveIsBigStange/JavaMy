c12c09c905a356555b684b2f8bd985a0de71aa78
LiveIsBigStange <98121339+LiveIsBigStange@users.noreply.github.com>
2023-10-09 09:01:56 +0300

    Added second week

---
+++ Second week/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //System.out.println("Hello world!");
+        //int age = scanner.nextInt();
+        //if(age >= 18){
+        //    System.out.println("adult");
+        //}else{
+        // System.out.println("child");
+        //}
+        //System.out.println("Finish");
+        int x = 109;
+        int v = scanner.nextInt();
+        int t = scanner.nextInt();
+        System.out.println();
+    }
+}
