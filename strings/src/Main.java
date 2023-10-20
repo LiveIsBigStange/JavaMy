6eaf76459eff6780437ae4a16b3e47e51adbae43
LiveIsBigStange <98121339+LiveIsBigStange@users.noreply.github.com>
2023-10-20 15:52:55 +0300

    added

---
+++ strings/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        //s.length()
+        //s.charAt(index) - find the symbol of string
+        //s.equals("") - s == ""
+        //s.toUpperCase(), s.toLowerCase()
+        //char ch = 'a';
+        //(int)'2' = 50 by ASCII
+        //(char)60 = 'a' by ASCII
+        //(char)8 = backspace by ASCII
+
+        //1
+        boolean pass = false;
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) >= (int)'a' && s.charAt(i) <= (int)'z'){
+                pass = true;
+            }
+            if (s.charAt(i) >= (int)'A' && s.charAt(i) <= (int)'Z'){
+                pass = true;
+            }
+            if (s.charAt(i) >= (int)'0' && s.charAt(i) <= (int)'9'){
+                pass = true;
+            }
+        }
+        System.out.print(pass);
+    }
+}
